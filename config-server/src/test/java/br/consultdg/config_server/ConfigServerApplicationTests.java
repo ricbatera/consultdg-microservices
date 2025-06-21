a67f47cffb9e3702b841270b3ce06fc7b0802382
Ricardo Alves Roberto <ricardo.roberto@rdrtech.com.br>
2025-06-21 08:47:32 -0300

    adicionado o config server, para prover properties centralizadas para todos MS

---
+++ config-server/src/test/java/br/consultdg/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.consultdg.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
